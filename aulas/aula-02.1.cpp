6a950a6cbdabc17af78de0c0f69bba16220d5f90
unknown <mateusleviaraujo@gmail.com>
2026-03-25 11:55:54 -0300

    blabla

--- aulas/aula-02.1.cpp
+++ aulas/aula-02.1.cpp
@@ -2,10 +2,32 @@
 using namespace std;
 
 int main() {
-    cout << "Querido usuário por favor digite um numero inteiro\n";
-    int input = 0;
+    cout << "Querido usuario por favor digite um numero inteiro\n";
+    int first = 0;
+    cin >> first;
 
 
+    int second = 0;
+    cout << "Digite outro número.";
+    cin >> second;
+
+    int operatorToCalc = 0;
+
+    cout << "Defina o operador para realizar a conta:\n";
+    cout << "1. +\n";
+    cout << "2. -\n";
+    cout << "3. *\n";
+    cout << "4. /\n";
+
+    switch (expression)
+    {
+    case constant expression:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
 
     return 0;
 }
