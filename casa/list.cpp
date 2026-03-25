016e3e864023fb6644ca0f9b3a420bc8e5c45a38
unknown <mateusleviaraujo@gmail.com>
2026-03-25 11:43:03 -0300

    aula 2

--- casa/list.cpp
+++ casa/list.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
+#include <map>
 #include <list>
 
 using namespace std;
+
+string ePar(int n) {
+    map<int, string> texto = {{0, "Verdadeiro"}, {1, "Falso"}};
+
+    return texto[n % 2];
+} 
 
 int somatorio(list<int> lista) {
     int total = 0;
 
     for(int n : lista) {
         total = total + n;
+
+        cout << "Numero atual: " << n << "\n";
+        cout << "E par: " << ePar(n) << "\n";
     }
 
     return total;
@@ -23,7 +33,7 @@ int main() {
     list<int> numeros = {1,2,3,4,5};
     int resultado = media(numeros);
 
-    cout << "Resultado: " << resultado;
+    cout << "Resultado medio: " << resultado;
 
     return 0;
 }
